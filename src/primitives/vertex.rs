5af4e4a1e8fdfdb97412b7db9329659f381d6afa
null-char <skp.frl@gmail.com>
2021-03-16 14:29:56 +0530

    refactor: move vertex buffer description to struct impl

--- src/primitives/vertex.rs
+++ src/primitives/vertex.rs
@@ -7,5 +7,26 @@ pub struct Vertex {
     pub color: [f32; 3],
 }
 
+impl Vertex {
+    pub fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            step_mode: wgpu::InputStepMode::Vertex,
+            array_stride: std::mem::size_of::<Vertex>() as u64,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    shader_location: 0,
+                    offset: 0,
+                    format: wgpu::VertexFormat::Float2,
+                },
+                wgpu::VertexAttribute {
+                    shader_location: 1,
+                    offset: std::mem::size_of::<[f32; 2]>() as u64,
+                    format: wgpu::VertexFormat::Float3,
+                },
+            ],
+        }
+    }
+}
+
 unsafe impl Pod for Vertex {}
 unsafe impl Zeroable for Vertex {}
