dfa1a206638cff1206435a2f45a924f81ed50690
null-char <skp.frl@gmail.com>
2021-04-18 12:31:28 +0530

    feat: add constants for bounding box and quadtree

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,14 @@
-// Constants
+use crate::primitives::scalar::Scalar;
 
 // 6.674 x 10^-11
-pub static G: f32 = 6.674e-11;
+pub static G: Scalar = 6.674e-11;
+
+// bounding_box
+// World space axes constants
+pub static MIN_X: Scalar = 0.0;
+pub static MAX_X: Scalar = 1000.0;
+pub static MIN_Y: Scalar = 0.0;
+pub static MAX_Y: Scalar = 1000.0;
+
+// quadtree
+pub static QUADTREE_CAPACITY: usize = 2;
